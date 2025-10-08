9ada593e00cab5de6b1daa5f98e55129fe16b331
duckycoding-dev <duckycoding.dev@gmail.com>
2025-10-08 19:54:28 +0200

    place static pikachu url

--- src/components/errors/pikachu-with-message.tsx
+++ src/components/errors/pikachu-with-message.tsx
@@ -1,6 +1,4 @@
 import Image from 'next/image';
-import { getPokemonByName } from '../../features/pokemon/pokemon.service';
-
 export async function PikachuWithMessage({ children }: { children?: React.ReactNode }) {    
   return (
     <div className='flex flex-col md:flex-row items-center gap-4'>
@@ -10,19 +8,17 @@ export async function PikachuWithMessage({ children }: { children?: React.ReactN
 };
 
 PikachuWithMessage.PikachuImage = async function PikachuImage({flipHorizontally = false}: {flipHorizontally?: boolean}) {
-  const pikachu = await getPokemonByName('pikachu');
-
 
   return (
     <div className={`${flipHorizontally ? 'scale-x-[-1]' : ''} w-[500px] md:max-w-[250px]`}>
       <div className='h-[250px] w-[500px] overflow-hidden md:-ml-[45%]'>
-        <Image src={pikachu?.sprites.back_default ?? ''} alt={pikachu?.name ?? 'pikachu'} width={500} height={500} className={`-mt-[21.5%] grayscale-50`} />  
+        <Image src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png'} alt={'pikachu'} width={500} height={500} className={`-mt-[21.5%] grayscale-50`} />  
       </div>
       <div className='h-[75px] w-[500px] grid grid-cols-1 overflow-hidden md:-ml-[45%]' >
         <div className="row-start-1 row-end-1 col-start-1 col-end-1 h-[100px] w-full z-10" style={{
           background: 'radial-gradient(farthest-corner at 50% top,rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 50%)',
         }}></div>
-        <Image src={pikachu?.sprites.back_default ?? ''} alt={pikachu?.name ?? 'pikachu shadow'} width={500} height={500} className={'row-start-1 row-end-1 col-start-1 col-end-1 -mt-[28.5%] rotate-x-180 grayscale-50 blur-[1px]'} />
+        <Image src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png'} alt={'pikachu shadow'} width={500} height={500} className={'row-start-1 row-end-1 col-start-1 col-end-1 -mt-[28.5%] rotate-x-180 grayscale-50 blur-[1px]'} />
       </div>
     </div>
   )
